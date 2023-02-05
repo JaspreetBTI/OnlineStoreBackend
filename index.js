fcfe8c79a8b173636ddd45813cf17268da63ee08
Jaspreet <jaspreetsandhu1994@gmail.com>
2023-02-05 15:53:44 +1100

    Added Structure

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+//start server
+app.listen(5000, () => {
+    console.log("Server started on http://localhost:5000");
+  });
+  
