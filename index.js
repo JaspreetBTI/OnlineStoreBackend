c23a9bde468bb6c4d5fb3f3ea703dfa6ef130135
Jaspreet <jaspreetsandhu1994@gmail.com>
2023-02-05 15:54:25 +1100

    Added Database Support

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const ObjectID = require("mongodb").ObjectID;
+// get properties from file
+let propertiesReader = require("properties-reader");
+let properties = propertiesReader(
+  path.resolve(__dirname, "conf/database.properties")
+);
+//encdoing URI for creating DataBase URI
+let dbPrefix = properties.get("db.prefix");
+let dbUsername = encodeURIComponent(properties.get("db.user"));
+let dbPwd = encodeURIComponent(properties.get("db.pwd"));
+let dbName = properties.get("db.dbName");
+let dbUrl = properties.get("db.dbUrl");
+let dbParams = properties.get("db.params");
+const uri = dbPrefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+// creating APP
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("short"));
 
 //start server
 app.listen(5000, () => {
